ed4ce23782bb79d548e29fac3e6a3f85584b1811
liudonghuan <ldhlfzysys@163.com>
2016-03-04 10:48:55 +0800

    addIconToHome增加本地服务器方式

--- AddIconToHome/AppDelegate.m
+++ AddIconToHome/AppDelegate.m
@@ -17,7 +17,6 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
     self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
     [self.window makeKeyAndVisible];
     ViewController *baseViewController = [[ViewController alloc]init];
--- AddIconToHome/ViewController.m
+++ AddIconToHome/ViewController.m
@@ -7,23 +7,35 @@
 //
 
 #import "ViewController.h"
-
+#import "HTTPServer.h"
+#import "DDLog.h"
+#import "DDTTYLogger.h"
 
 @interface ViewController ()
-
+{
+    HTTPServer *httpServer;
+}
 @end
+
+static const int ddLogLevel = LOG_LEVEL_VERBOSE;
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
     self.view.backgroundColor = [UIColor whiteColor];
-    UIButton *shareIcon = [[UIButton alloc]initWithFrame:CGRectMake(50, 50, 50, 50)];
-    shareIcon.backgroundColor = [UIColor redColor];
-    [self.view addSubview:shareIcon];
     
+    UIButton *shareIcon = [[UIButton alloc]initWithFrame:CGRectMake(10, 50, 150, 50)];
+    [shareIcon setTitle:@"web在服务器" forState:UIControlStateNormal];
+    [shareIcon setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [self.view addSubview:shareIcon];
     [shareIcon addTarget:self action:@selector(shareClick) forControlEvents:UIControlEventTouchUpInside];
+    
+    UIButton *shareIconLocal = [[UIButton alloc]initWithFrame:CGRectMake(170, 50, 150, 50)];
+    [shareIconLocal setTitle:@"web在本地" forState:UIControlStateNormal];
+    [shareIconLocal setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [self.view addSubview:shareIconLocal];
+    [shareIconLocal addTarget:self action:@selector(shareLocalClick) forControlEvents:UIControlEventTouchUpInside];
 }
 
 - (void)shareClick
@@ -33,9 +45,45 @@
     [[UIApplication sharedApplication ] openURL: url];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)shareLocalClick
+{
+    // Configure our logging framework.
+    // To keep things simple and fast, we're just going to log to the Xcode console.
+    [DDLog addLogger:[DDTTYLogger sharedInstance]];
+    
+    // Create server using our custom MyHTTPServer class
+    httpServer = [[HTTPServer alloc] init];
+    
+    // Tell the server to broadcast its presence via Bonjour.
+    // This allows browsers such as Safari to automatically discover our service.
+    [httpServer setType:@"_http._tcp."];
+    
+    // Normally there's no need to run our server on any specific port.
+    // Technologies like Bonjour allow clients to dynamically discover the server's port at runtime.
+    // However, for easy testing you may want force a certain port so you can just hit the refresh button.
+    [httpServer setPort:12345];
+    
+    // Serve files from our embedded Web folder
+    NSString *webPath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"Web"];
+    DDLogInfo(@"Setting document root: %@", webPath);
+    
+    [httpServer setDocumentRoot:webPath];
+    
+    [self startServer];
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"http://127.0.0.1:12345"]];
+}
+
+- (void)startServer
+{
+    NSError *error;
+    if([httpServer start:&error])
+    {
+        DDLogInfo(@"Started HTTP Server on port %hu", [httpServer listeningPort]);
+    }
+    else
+    {
+        DDLogError(@"Error starting HTTP Server: %@", error);
+    }
 }
 
 @end
