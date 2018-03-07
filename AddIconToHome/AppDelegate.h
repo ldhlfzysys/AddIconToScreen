8c6b54c57c74832827daf153ab440717db6782f2
ldhlfzysys <ldhlfzysys@163.com>
2018-03-07 19:55:48 +0800

    应用1秒保活

--- AddIconToHome/AppDelegate.h
+++ AddIconToHome/AppDelegate.h
@@ -12,6 +12,7 @@
 
 @property (strong, nonatomic) UIWindow *window;
 
+@property (nonatomic , assign) UIBackgroundTaskIdentifier backgroundUpdateTask;
 
 @end
 
--- AddIconToHome/AppDelegate.m
+++ AddIconToHome/AppDelegate.m
@@ -30,8 +30,21 @@
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    [self beginBackgroundUpdateTask];
+    sleep(1);
+    [self endBackGroundUpdateTask];
+}
+
+- (void)beginBackgroundUpdateTask{
+    self.backgroundUpdateTask = [[UIApplication sharedApplication] beginBackgroundTaskWithExpirationHandler:^{
+        [self endBackGroundUpdateTask];
+    }];
+    
+}
+
+- (void)endBackGroundUpdateTask{
+    [[UIApplication sharedApplication] endBackgroundTask:self.backgroundUpdateTask];
+    self.backgroundUpdateTask = UIBackgroundTaskInvalid;
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
